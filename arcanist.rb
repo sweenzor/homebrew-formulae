0cf1d83de25997876bbe8bf198c98aa7df9eb220
Matt Sweeney <mattswe@gmail.com>
2013-12-12 18:04:46 -0800

    Create arcanist.rb

---
+++ arcanist.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Arcanist < Formula
+  homepage 'http://phabricator.org'
+  url 'https://github.com/facebook/arcanist.git'
+  version 'head'
+
+  def install
+
+    # clone arcanist
+    system 'git', 'clone', url
+
+    # clone libphutil into arcanist's includes
+    cd 'arcanist/externals/includes' do
+      system 'git', 'clone', 'https://github.com/facebook/libphutil.git'
+    end
+
+    # copy the whole cloned dir (including includes) to Cellar
+    prefix.install 'arcanist'
+
+    # symlink arc's bin to where homebrew expects a bin
+    bin.install_symlink prefix + 'arcanist/bin/arc'
+
+  end
+
+end
