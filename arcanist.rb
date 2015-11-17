6294b991e2becd73bfbd4dabec8bba234e620f7a
Matt Sweeney <mattswe@gmail.com>
2015-11-17 15:15:42 -0500

    Specify download strategy

--- arcanist.rb
+++ arcanist.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Arcanist < Formula
   homepage 'http://phabricator.org'
-  url 'https://github.com/phacility/arcanist.git'
+  url 'https://github.com/phacility/arcanist.git', :using => :git
   version 'head'
 
   def install
